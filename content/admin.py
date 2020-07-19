78d32d368aa11b9565ae98405b2c4acbd38e5a44
slymnaltunakar <suleymanaltunakar@gmail.com>
2020-07-19 10:00:42 +0300

    ilk yükleme

---
+++ content/admin.py
@@ -0,0 +1,19 @@
+from django.contrib import admin
+
+# Register your models here.
+from django.contrib import admin
+
+from .models import Category, Product
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['title', 'status']
+    list_filter = ['status']
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['title', 'category', 'status']
+    list_filter =  ['category']
+
+
+admin.site.register(Category,CategoryAdmin)
+admin.site.register(Product,ProductAdmin)
