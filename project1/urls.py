1bc24fe4db6fa23d6e2c73be5d880f974133fa9a
slymnaltunakar <suleymanaltunakar@gmail.com>
2020-07-20 17:26:31 +0300

    ckeditoradminpanel

--- project1/urls.py
+++ project1/urls.py
@@ -22,6 +22,7 @@ urlpatterns = [
     path('', include('home.urls')),
     path('content/', include('content.urls')),
     path('admin/', admin.site.urls),
+    path('ckeditor/', include('ckeditor_uploader.urls')),
 ]
 if settings.DEBUG: # new
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
