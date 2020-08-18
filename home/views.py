5b5b801d8902e197b53c236fe49df3e3349db5bd
slymnaltunakar <suleymanaltunakar@gmail.com>
2020-08-18 22:10:58 +0300

    search butonu

--- home/views.py
+++ home/views.py
@@ -1,11 +1,15 @@
+import json
+
 from django.core.checks import messages
 from django.core.mail import message
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 
 
+
 # Create your views here.
 from content.models import Product, Category, Images, Comment
+from home.forms import SearchForm
 from home.models import Setting, ContactFormumuz, ContactForm
 
 
@@ -69,3 +73,19 @@ def product_detail(request,id,slug):
     images= Images.objects.filter(icerik_id=id)
     context = {'product': product,'category': category, 'slug': slug,'images': images,'comments': comments}
     return render(request, 'product_detail.html',context)
+
+def product_search(request):
+    if request.method == 'POST':
+        form=SearchForm(request.POST)
+        if form.is_valid():
+            category=Category.objects.all()
+            query=form.cleaned_data['query'] #formdan bilgiyi al
+            products=Product.objects.filter(title__icontains=query) #select from like query
+            context={
+                'products':products,
+                'category':category,
+            }
+            return render(request,'products_search.html',context)
+    return  HttpResponseRedirect('/')
+
+
